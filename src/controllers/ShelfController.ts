0fcb2b48a0218f1664cf8f04b12be6beae6c21d6
root <root@DESKTOP-SINR802.localdomain>
2020-05-17 23:59:38 -0300

    Changed the shelfcontroller to get game

--- src/controllers/ShelfController.ts
+++ src/controllers/ShelfController.ts
@@ -3,56 +3,7 @@ import Shelf, { ShelfInterface } from '../models/Shelf';
 
 class ShelfController {
   async store(request: Request, response: Response): Promise<void> {
-    const game = {
-      id: 3333,
-      cover: {
-        id: 85867,
-        height: 960,
-        url: '//images.igdb.com/igdb/image/upload/t_thumb/co1u97.jpg',
-        width: 720,
-      },
-      involved_companies: [
-        {
-          id: 24212,
-          company: {
-            id: 248,
-            name: 'Bandai Namco Entertainment',
-          },
-        },
-        {
-          id: 24213,
-          company: {
-            id: 2438,
-            name: 'CyberConnect2',
-          },
-        },
-      ],
-      name: 'Naruto Shippuden: Ultimate Ninja Storm 4',
-      platforms: [
-        {
-          id: 6,
-          name: 'PC (Microsoft Windows)',
-        },
-        {
-          id: 48,
-          name: 'PlayStation 4',
-        },
-        {
-          id: 49,
-          name: 'Xbox One',
-        },
-      ],
-      popularity: 5.903648259672391,
-      rating_count: 61,
-      summary:
-        'Experience the exhilarating full-adventure Naruto Shippuden and follow Naruto Uzumaki on all his fights.\n\nWith more than 12 million Naruto Shippuden Ultimate Ninja STORM games sold worldwide, this series established itself among the pinnacle of Anime & Manga adaptations on videogames! As every good story comes to an end Naruto Shippuden: Ultimate Ninja Storm 4 is going to be the ultimate STORM game! For the first time ever, a Naruto/Naruto Shippuden game will take advantage of the graphics power of the new generation of consoles.',
-      themes: [
-        {
-          id: 1,
-          name: 'Action',
-        },
-      ],
-    };
+    const { game } = request.body;
 
     const checkExistShelf = await Shelf.findOne({
       _id: request.user.id,
